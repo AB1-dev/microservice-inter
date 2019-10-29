c7884002d042ed12d6be5386602601ff139fc336
Vamsi <vamsipv.desu37@live.com>
2019-10-29 22:03:00 +0530

    second commit

--- src/test/java/com/example/BankDetails/dao/BankDAOTest.java
+++ src/test/java/com/example/BankDetails/dao/BankDAOTest.java
@@ -43,51 +43,9 @@ class BankDAOTest {
 		java.util.List<Bank> getFrmDB = dao.getAllAccountDetails();
 		assertNotNull(getFrmDB.get(0).getAccountNumber());
 	}
-	
-	@Test
-	void testgetAccountById()
-	{
-		Bank bank = getAccountValues();
-		Bank setToDB = entitymanager.persist(bank);
-		Bank getFrmDB = dao.getAccount(bank.getAccountNumber());
-		assertEquals(setToDB.getAccountNumber(),getFrmDB.getAccountNumber());
-	}
-	
-	@Test
-	void testUpdateAccount()
-	{
-		Bank bank1 = new Bank(); 
-		//bank1.setId(1);
-		bank1.setAccountNumber(1238719872); 
-		bank1.setEmail("sandy@gmail.com");
-		bank1.setFirstName("sandeep");
-		bank1.setLastName("Manohar DAS");
-		bank1.setPhone("9090822"); 
-		bank1.setAddress1("cuttak");
-		bank1.setAddress2("orissa"); 
-		bank1.setAmount(235000);
-		
-		Bank bank = getAccountValues();
-		Bank setToDB = entitymanager.persist(bank);
-		Bank getFrmDB = dao.updateAccount(bank.getAccountNumber(), bank1);
-		assertEquals("9090822",getFrmDB.getPhone());
-	}
-	
-	@Test
-	void testDeleteAccount()
-	{
-		Bank bank = getAccountValues();
-		Bank bank1 = dao.createAccount(bank);
-		//Bank storeToDB = entitymanager.persist(bank);
-		Boolean getFrmDB = dao.deleteAccount(bank1.getAccountNumber());
-		
-		//assertNotNull(getFrmDB.getAccountNumber());
-		assertEquals(true, getFrmDB);
-	}
 
 	private Bank getAccountValues() { 
 		Bank bank = new Bank(); 
-		//bank.setId(1);
 		bank.setAccountNumber(1238719872); 
 		bank.setEmail("sandy@gmail.com");
 		bank.setFirstName("sandeep");
